b9bf41e85c4f4069b1ba695f697b2ff4a7590359
teffidev <tefyberal09@gmail.com>
2023-03-01 12:15:47 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Form from "./Components/Form/Form";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const Form = () => {
+    //Se crea un estado para cada input del form
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    //Se crea un manejador de eventos para cada uno de    los input
+    const onChangeUserName = (e) => setUserName(e.target.value);
+    const onChangePassword = (e) => setPassword(e.target.value);
+
+    //Se crea el manejador para el evento onSubmit
+    const onSubmitForm = (e) => {
+    //evitamos que se recargue la pagina, previniendo el comportamiento por defecto
+        e.preventDefault();
+
+    //Por ahora solo mostramos el nombre del usuario
+        aler(`Bienvenido: ${userName}`);
+    };
+
+    return (
+        <div>
+            <h3>Iniciar Sesión</h3>
+            <form onSubmit={onSubmitForm}>
+                <input
+                type="text"
+                placeholder="Nombre de usuario"
+                value={userName}
+                onChange={onChangeUserName}
+                />
+                <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={onChangePassword}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
